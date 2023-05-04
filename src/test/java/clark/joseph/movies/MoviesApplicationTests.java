5ae21b48b82af67210327b12884a30b10fc094ef
josephclark293 <josephclark293@gmail.com>
2023-05-04 13:17:14 -0400

    first commit

---
+++ src/test/java/clark/joseph/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package clark.joseph.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
